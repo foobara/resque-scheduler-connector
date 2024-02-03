bfc87192a93e2e5766b93bb0974d39ec08c12ac0
Miles Georgi <azimux@gmail.com>
2024-02-03 14:41:26 -0800

    Initialz commit, generated from RubyMine

---
+++ lib/foobara/resque_scheduler_connector.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "resque_scheduler_connector/version"
+
+module Foobara
+  module ResqueSchedulerConnector
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/foobara/resque_scheduler_connector_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Foobara::ResqueSchedulerConnector do
+  it "has a version number" do
+    expect(Foobara::ResqueSchedulerConnector::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
