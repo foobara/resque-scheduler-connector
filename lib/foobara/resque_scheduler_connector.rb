fb94451cf728b188e6d559ce772fc37eb7858931
Miles Georgi <azimux@gmail.com>
2024-02-03 17:33:27 -0800

    Implement .reset_all

--- lib/foobara/resque_scheduler_connector.rb
+++ lib/foobara/resque_scheduler_connector.rb
@@ -1,4 +1,15 @@
 require "foobara/all"
 require "foobara/command_connectors"
 
-Foobara::Util.require_directory "#{__dir__}/../../src"
+module Foobara
+  module ResqueSchedulerConnector
+    class << self
+      def reset_all
+        # TODO: protect against this in production
+        Resque.redis.flushdb
+      end
+    end
+  end
+end
+
+Foobara::Util.require_directory("#{__dir__}/../../src")
